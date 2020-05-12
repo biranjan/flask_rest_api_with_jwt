d651d131cb1830f63e6e76e8be1fad62c026b384
Biranjan <birajan.raut@gmail.com>
2020-05-12 22:54:14 +0300

    jwt feature enabled

--- resource/User.py
+++ resource/User.py
@@ -3,14 +3,17 @@ import sys
 sys.path.append('..')
 from models import UserModel, UserSchema
 from flask import Blueprint, Response, jsonify, request
-
+from shared.Authentication import Auth
+import json
 user_schema = UserSchema()
 
 class UserResource(Resource):
+    @Auth.auth_required
     def get(self):
         users = UserModel.query.all()
         return jsonify([user.serialize() for user in users])
-
+    
+    
     def post(self):
         json_data = request.get_json(force=True)
         if not json_data:
@@ -25,10 +28,17 @@ class UserResource(Resource):
             password = json_data['password']
         )
         new_user.save()
-        result = user_schema.dump(new_user)
 
-        return {"status": 'success', 'data': result}, 201
+        #result = user_schema.dump(new_user)
+        print(new_user)
+        print(new_user.id)
+        token = Auth.generate_token(new_user.id)
+        
+        return token
 
+        #return {"status": 'success', 'data': result}, 201
+    
+    @Auth.auth_required
     def put(self):
         json_data = request.get_json(force=True)
         if not json_data:
@@ -43,6 +53,17 @@ class UserResource(Resource):
         result = user_schema.dump(UserModel).data
 
         return {"status": 'success', 'data': result}, 204
-        
+
+    
+def custom_response(res, status_code):
+    """
+    Custom Response Function
+    """
+    return Response(
+        mimetype="application/json",
+        response=json.dumps(res),
+        status=status_code
+    )
+    
 
 
